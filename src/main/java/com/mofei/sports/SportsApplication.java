dd1862188fb2e23e1ef2f896431bb006128d184a
lambda8421 <lambda8421@163.com>
2018-07-26 21:59:20 -0400

    merge conflict

--- src/main/java/com/mofei/sports/SportsApplication.java
+++ src/main/java/com/mofei/sports/SportsApplication.java
@@ -1,10 +1,18 @@
 package com.mofei.sports;
 
+import com.mofei.sports.crawler.Crawler;
+import com.mofei.sports.web.entity.BasketballTeam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SportsApplication {
+
+    @Autowired
+    private static Crawler crawler;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsApplication.class, args);
