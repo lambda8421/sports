2a895d7c42df39d36c14d0059c35150eff0df0e6
lambda8421 <lambda8421@163.com>
2018-08-08 22:43:38 -0400

    add url list dic

--- src/main/java/com/mofei/sports/crawler/CrawlerRunner.java
+++ src/main/java/com/mofei/sports/crawler/CrawlerRunner.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class CrawlerRunner implements CommandLineRunner {
@@ -22,6 +25,8 @@ public class CrawlerRunner implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception {
+
+
         crawler.init("http://nba.win007.com/jsData/matchResult/17-18/l1_1_2017_10.js?version=2018072503)");
 
         saveDataService.saveBasketballTeams( CleansingData.getBasketballTeams(crawler.getCrawlerData()));
@@ -37,6 +42,30 @@ public class CrawlerRunner implements CommandLineRunner {
                             matchOddsCrawler.getCrawlerData(),
                             match.getMatchId()));
         }
+
+    }
+
+    public Map<String, List<String>> addTaskToQueue(){
+
+        Map<String,List<String>> seasons = new HashMap<>();
+        for (int i = 0; i < 18; i++) {
+            String prev = i < 10 ? "0"+i : i+"";
+            String next = (i+1) < 10 ? "0"+(i+1) : (i+1)+"";
+            String season = prev + "-" + next;
+
+            List<String> yearAndMonthOfSeason = new ArrayList<>();
+            for (int j = 10; j < 17; j++) {
+                String month = j > 12 ? "0"+(j-12):""+j;
+                String yearMonth = "20" + month;
+                yearAndMonthOfSeason.add(yearMonth);
+            }
+
+            seasons.put(season,yearAndMonthOfSeason);
+        }
+
+        return seasons;
+
+
 
     }
 }
