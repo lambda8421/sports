11ecf9f2ec84e732c0a3eff6a8a313c8e8929a6a
yongzhao <lambda8421@163.com>
2018-08-14 13:37:30 -0400

    Add the user

--- src/main/java/com/mofei/sports/web/controller/UserController.java
+++ src/main/java/com/mofei/sports/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mofei.sports.web.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +14,43 @@ public class UserController {
 
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @RequestMapping(value = "/login",method = RequestMethod.POST,
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
     @ResponseBody
-    public String login( @RequestBody LoginForm loginForm){
+    public LoginForm login( @RequestBody LoginForm loginForm){
 
 
         System.out.println(loginForm.toString());
-        return "xx";
+        return loginForm;
+    }
+}
+
+class LoginForm{
+    String username;
+    String password;
+    String scores;
+
+    public LoginForm() {
     }
 
-    class LoginForm{
-        String username;
-        String password;
+    public String getUsername() {
+        return username;
+    }
 
-        @Override
-        public String toString() {
-            return super.toString() + username + password + "dd";
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + username + password + "dd";
     }
 }
